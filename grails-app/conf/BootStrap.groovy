aad3c1d98345ec153f54f694962fbb07da5c8d2a
Hubert Klein Ikkink <h.kleinikkink@gmail.com>
2010-11-13 11:48:52 +0100

    Added default users

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,17 @@
+import com.vx.meetup.User
+import com.vx.meetup.UserRole
+import com.vx.meetup.Role
+
 class BootStrap {
 
+    def springSecurityService
+
     def init = { servletContext ->
+        def role = new Role(authority: 'ROLE_USER').save()
+        ['marc', 'michel', 'mrhaki'].each {
+            def user = new User(username: it, password: springSecurityService.encodePassword(it), enabled: true).save()
+            UserRole.create user, Role.findByAuthority('ROLE_USER'), true
+        }
     }
     def destroy = {
     }
