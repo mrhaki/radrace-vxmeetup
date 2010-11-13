49a4da1553cc2d1b56768f2a9bbf2939810bbff8
Hubert Klein Ikkink <h.kleinikkink@gmail.com>
2010-11-13 13:53:15 +0100

    Added meetup

--- grails-app/domain/com/vx/meetup/Meetup.groovy
+++ grails-app/domain/com/vx/meetup/Meetup.groovy
@@ -2,6 +2,13 @@ package com.vx.meetup
 
 class Meetup {
 
+    String titel
+    String omschrijving
+    Date datum
+
     static constraints = {
+        titel blank: false, nullable: false
+        omschrijving blank: false, nullable: false
+        datum blank: false, nullable: false
     }
 }
