55313ee3e1bb8bb3b77ab452d617b81cfa4b2346
Michel Vollebregt <michel@Laptop-Michel.local>
2010-11-13 15:43:16 +0100

    Opslag naar filesysteem, navbar uit aantal schermen gehaald

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -19,28 +19,40 @@ class BootStrap {
     }
 
     private void initUsers(it) {
-        def role = new Role(authority: 'ROLE_USER').save()
-        ['marc', 'michel', 'mrhaki'].each {
-            def user = new User(username: it, password: springSecurityService.encodePassword(it), enabled: true).save()
-            UserRole.create user, Role.findByAuthority('ROLE_USER'), true
+        if (User.count() == 0) {
+            def role = new Role(authority: 'ROLE_USER').save()
+            ['marc', 'michel', 'mrhaki'].each {
+                def user = new User(username: it, password: springSecurityService.encodePassword(it), enabled: true).save()
+                UserRole.create user, Role.findByAuthority('ROLE_USER'), true
+            }
         }
     }
 
     private void initMeetingGroups() {
-        ['Architectuur', 'Grails', 'SEAM', 'Coding', 'Agile SIG', 'Scala'].each {
-            def group = new MeetingGroep(titel: it, omschrijving: it).save(flush: true)
-            println "$group is created"
+        if (MeetingGroep.count() == 0) {
+            ['Architectuur', 'Grails', 'SEAM', 'Coding', 'Agile SIG', 'Scala'].each {
+                def group = new MeetingGroep(titel: it, omschrijving: it).save(flush: true)
+                println "$group is created"
+            }
         }
     }
 
     private void initMeetups() {
-        createMeetup titel: 'Agile SIG', omschrijving: 'T.O.C. Demming Cycle', datum: new Date(year: 110, month: 11, date: 19)
-        createMeetup titel: 'Archictuur SIG', omschrijving: 'Design Patterns', datum: new Date(year: 110, month: 11, date: 19)
-        createMeetup titel: 'Gails SIG', omschrijving: 'TDD', datum: new Date(year: 111, month:0, date: 19)
-        createMeetup titel: 'Scala Usergroup', omschrijving: 'LIFT', datum: new Date(year: 111, month: 1, date: 19)
-        createMeetup titel: 'SEAM SIG', omschrijving: 'IRIS', datum: new Date(year: 111, month: 1, date: 24)
-        createMeetup titel: 'Coding Dojo', omschrijving: 'SETS', datum: new Date(year: 111, month: 1, date: 24)
-        createMeetup titel: 'Java weekend', omschrijving: 'Weekend fun', datum: new Date(year: 110, month: 10, date: 12)
+        if (Meetup.count() == 0) {
+            createMeetup titel: 'Agile SIG', omschrijving: 'T.O.C. Demming Cycle', datum: createDate(year: 110, month: 11, date: 19)
+            createMeetup titel: 'Archictuur SIG', omschrijving: 'Design Patterns', datum: createDate(year: 110, month: 11, date: 19)
+            createMeetup titel: 'Gails SIG', omschrijving: 'TDD', datum: createDate(year: 111, month:0, date: 19)
+            createMeetup titel: 'Scala Usergroup', omschrijving: 'LIFT', datum: createDate(year: 111, month: 1, date: 19)
+            createMeetup titel: 'SEAM SIG', omschrijving: 'IRIS', datum: createDate(year: 111, month: 1, date: 24)
+            createMeetup titel: 'Coding Dojo', omschrijving: 'SETS', datum: createDate(year: 111, month: 1, date: 24)
+            createMeetup titel: 'Java weekend', omschrijving: 'Weekend fun', datum: createDate(year: 110, month: 10, date: 12)
+        }
+    }
+
+    private Date createDate(args) {
+        Date date = new Date(args)
+        date.clearTime()
+        return date
     }
 
     private void createMeetup(args) {
