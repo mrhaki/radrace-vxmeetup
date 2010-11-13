689cb1980ea3a999bfd587850894653c205c7e63
Michel Vollebregt <michel@Laptop-Michel.local>
2010-11-13 12:42:41 +0100

    Meetinggroep constraints

--- grails-app/domain/com/vx/meetup/MeetingGroep.groovy
+++ grails-app/domain/com/vx/meetup/MeetingGroep.groovy
@@ -3,6 +3,8 @@ package com.vx.meetup
 class MeetingGroep {
 
     static constraints = {
+        titel         blank: false, nullable: false
+        omschrijving  blank: false, nullable: false
     }
 
     String titel;
