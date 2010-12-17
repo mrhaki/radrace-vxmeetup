045d91a3e6a2e0cb53bb73852f35169d3393673b
Hubert Klein Ikkink <h.kleinikkink@gmail.com>
2010-12-17 09:07:56 +0100

    Code-coverage en GMetrics plugins toegevoegd.

--- server/grails-app/conf/BuildConfig.groovy
+++ server/grails-app/conf/BuildConfig.groovy
@@ -2,6 +2,7 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -23,9 +24,22 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
+
+    plugins {
+        build(':gmetrics:0.3') {
+            transitive = false  // Includes dependency on groovy-all 1.6.9 which conflicts with Spock in IDEA.
+        }
+
+    }
+
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        // runtime 'mysql:mysql-connector-java:5.1.5'
+        // Because at the plugins section we used transitive = false we must here define the GMetrics ANT task dependency.
+        build('org.gmetrics:GMetrics:0.3') {
+            transitive = false  // Includes dependency on groovy-all 1.6.9 which conflicts with Spock in IDEA.
+        }
+
     }
+
 }
